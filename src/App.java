2f269fc81447d0742c60ce37a90c7fc50a0a070f
AnguloJairo <jairo.angulo@epn.edu.ec>
2024-08-05 22:29:50 -0500

    hola

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        //Splash
+        SplashScreenForm.show();
+
+
+        
+        Sistema sistema = new Sistema();
+        sistema.mostrarMenu();
+        Usuario usuario = new Usuario();
+        usuario.login();
+
+    }
+}
---
+++ src/Monitoreo.java
@@ -0,0 +1,5 @@
+public class Monitoreo {
+    public String enviarNotificacion(Usuario usuario, String mensaje) {
+        return mensaje;
+    }
+}
